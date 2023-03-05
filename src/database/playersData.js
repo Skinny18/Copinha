d69dc8efb013100d377250c766dee3e16ea995d1
Gabriel Carlos Carvalho <gabrielcarlos@MacBook-Pro-de-Gabriel.local>
2023-03-05 11:47:44 -0400

    time

--- src/database/playersData.js
+++ src/database/playersData.js
@@ -1,7 +1,7 @@
 const playerList = ["Hudson", "Edson", "Leonardo", "Maranhao", "Abraao", "Erick", "Rian", "Matheus Valerio"]
 const playerList2 = ["Poupinho", "Valdercley", "Matheus", "Lucas Matheus", "Kauan", "Biel", "Fellipe", "Ayrton"]
 const playerList3 = ["Thiago Silva", "Nicolas Gemeos", "Joao Gemeos", "Thiago(Irmao Ayrton)", "Barriga", "Vinicius", "Samuel", "Joao Vitor"]
-const selecaoList = ["Brasil", "Argentina", "Alemanha", "França", "Inglaterra", "Nigéria", "Costa Rica", "Croácia"] 
+const selecaoList = ["Aston Vila", "Crystal Palace", "Leicester", "Bournemounth", "Southampton", "West Ham", "Brighton", "New Castle"] 
 
 
 export { playerList, playerList2, playerList3, selecaoList };
