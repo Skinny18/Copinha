fad88c575bc8ddfd00f032921ac278ffba0a2919
Gabriel Carlos Carvalho <gabrielcarlos@MacBook-Pro-de-Gabriel.local>
2023-03-10 13:19:11 -0400

    commit

--- src/database/playersData.js
+++ src/database/playersData.js
@@ -1,5 +1,5 @@
-const playerList = ["Hudson", "Edson", "Leonardo", "Maranhao", "Abraao", "Erick", "Rian", "Matheus Valerio"]
-const playerList2 = ["Poupinho", "Valdercley", "Matheus", "Lucas Matheus", "Kauan", "Biel", "Fellipe", "Ayrton"]
+const playerList = ["Hudson", "Edson", "Leonardo", "Maranhao", "Valdercley", "Erick", "Rian", "Matheus Valerio"]
+const playerList2 = ["Poupinho", "Lucas Henrique", "Matheus", "Lucas Matheus", "Kauan", "Biel", "Fellipe", "Ayrton"]
 const playerList3 = ["Thiago Silva", "Nicolas Gemeos", "Joao Gemeos", "Thiago(Irmao Ayrton)", "Barriga", "Vinicius", "Samuel", "Joao Vitor"]
 const selecaoList = ["Aston Vila", "Crystal Palace", "Leicester", "Bournemounth", "Southampton", "West Ham", "Brighton", "New Castle"] 
 
