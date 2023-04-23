89832f2435aacf9b2b9c76bda6137d04d5ea009e
Gabriel Carlos Carvalho <gabrielcarlos@MacBook-Pro-de-Gabriel.local>
2023-04-23 12:28:06 -0400

    branco

--- src/database/playersData.js
+++ src/database/playersData.js
@@ -1,6 +1,6 @@
 const playerList = ["Hudson", "Edson", "Leonardo", "Maranhao", "Fernando", "Caixa", "Baixinho", "Matheus Valerio"]
 const playerList2 = ["Rian", "Valdercley", "Matheus", "Lucas Matheus", "Joao Vitor", "Barriga", "Fellipe", "Ayrton"]
-const playerList3 = ["Thiago Silva", "Nicolas Gemeos", "Joao Gemeos", "Thiago(Irmao Ayrton)", "Biel", "Kaua", "Madson", "Lucas Henrique"]
+const playerList3 = ["Thiago Silva", "Nicolas Gemeos", "Joao Gemeos", "Thiago(Irmao Ayrton)", "Biel", "Kaua", "Branco", "Lucas Henrique"]
 const selecaoList = ["Flamengo", "Palmeiras", "Corinthians", "Gremio", "Internacional", "Fluminense", "Santos", "São Paulo"] 
 
 
